ca36e4a2597b84550816a1b80f4f750359476d10
Francisco <francisco.tun@avocadoblock.com>
2022-03-01 22:15:00 -0600

    unpin IPFS

--- js/upload_ipfs.js
+++ js/upload_ipfs.js
@@ -1,21 +1,13 @@
-// create a request to 'https://api.pinata.cloud/pinning/pinFileToIPFS' using post method whith headers 'pinata_api_key', 'pinata_secret_api_key' and body 'file', 'pinataMetadata' and 'pinataOptions'
-// return the response
-// console.log(response);
-
-
 const pinFileToIPFS = async () => {
   let file = document.getElementById('fileinput').files[0];
   const url = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
 
   let data = new FormData();
-  // let fr = new FileReader();
-  // data.append('file', fr.readAsBinaryString(file));
   data.append('file', file);
-
-  
+  console.log(file);
 
   const metadata = JSON.stringify({
-    name: 'nombre',
+    name: file.name,
     keyvalues: {
       exapmpleKey: 'exampleValue',
     }
@@ -41,17 +33,6 @@ const pinFileToIPFS = async () => {
 
   data.append('pinataOptions', pinataOptions);
 
-  // const response = await fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     pinata_api_key: 'ba65da047212926c9ee4',
-  //     pinata_secret_api_key: '3897644a77bf3ea3836a17fa1bed1c8897f95e5f148be0728e2f73c48aa7baec'
-  //   },
-  //   body: data
-  // });
-  // return response.json();
-
   return axios
       .post(url, data, {
         maxBodyLength: 'Infinity',
@@ -62,6 +43,11 @@ const pinFileToIPFS = async () => {
         }
       }).then(function (response) {
         console.log(response);
+        let linkIPFS = document.getElementById('linkIPFS');
+        // set href of linkIPFS
+        linkIPFS.href = "https://gateway.pinata.cloud/ipfs/"+response.data.IpfsHash;
+        let uri = document.getElementById('upload-status');
+        uri.innerHTML = "https://gateway.pinata.cloud/ipfs/"+response.data.IpfsHash;
       })
       .catch(function (error) {
           console.log(error);
@@ -69,21 +55,26 @@ const pinFileToIPFS = async () => {
 
 }
 
+const unpinFileFromIPFS = async () => {
+  let hash = document.getElementById('hash').value;
+  const url = 'https://api.pinata.cloud/pinning/unpin/'+hash;
+
+  return axios
+        .delete(url, {
+            headers: {
+                pinata_api_key: 'ba65da047212926c9ee4',
+                pinata_secret_api_key: '3897644a77bf3ea3836a17fa1bed1c8897f95e5f148be0728e2f73c48aa7baec'
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+            //handle response here
+        })
+        .catch(function (error) {
+            console.log(error);
+            //handle error here
+        });
+}
+
 document.getElementById("upload").addEventListener("click", pinFileToIPFS);
-
-
-
-// const getUsers = () => {
-//   let fr = new FileReader();
-//   fr.readAsBinaryString(document.getElementById('fileinput').files[0]);
-//   axios.get('https://reqres.in/api/users')
-//   .then(response => {
-//    const users = response.data.data;
-//    console.log(`GET users`, users);
-//  })
-//   .catch(error => console.error(error));
-//  };
-
-//  getUsers();
-
-// document.getElementById("upload").addEventListener("click", getUsers);
+document.getElementById('delete').addEventListener('click', unpinFileFromIPFS);
